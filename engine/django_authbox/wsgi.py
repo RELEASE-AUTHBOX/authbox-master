3bde4f60e8f4f010ce9ca298699fc6c6e234c5ec
manjaro-linux <suratiwan03@gmail.com>
2023-08-15 10:43:40 +0800

    Change wsgi setting

--- engine/django_authbox/wsgi.py
+++ engine/django_authbox/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_outbox.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_authbox.settings')
 
 application = get_wsgi_application()
