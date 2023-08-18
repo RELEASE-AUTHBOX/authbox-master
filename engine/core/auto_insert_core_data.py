76ef094b1f99cb3f81f82a8f2b77c9d2b17abfa8
manjaro-linux <suratiwan03@gmail.com>
2023-08-18 11:42:02 +0800

    Update core data default menu

--- engine/core/auto_insert_core_data.py
+++ engine/core/auto_insert_core_data.py
@@ -1,43 +1,56 @@
-_C='defaults'
-_B='condition'
+_D='defaults'
+_C='condition'
+_B='r'
 _A='id'
 import json,os
 from allauth.socialaccount.models import SocialApp
 from menu.models import Menu,MenuGroup
-from .models import ModelList,ModelListSetting,Template,TemplateOwner
+from .models import MenuDefault,ModelList,ModelListSetting,Template,TemplateOwner
+color_done='\x1b[94m'+'DONE  '+'\x1b[0m'
 def create_template_owner(file_path):
 	A=[]
-	with open(os.path.join(file_path,'template_owner.json'),'r')as C:A=json.load(C)
-	for B in A:D,E=TemplateOwner.objects.update_or_create(id=B[_B][_A],defaults=B[_C])
-	print('Done Write data Template Owner to database!')
+	with open(os.path.join(file_path,'template_owner.json'),_B)as D:A=json.load(D)
+	B=1;E=len(A)
+	for C in A:F=B/E*100;print(f"Writing [Template Owner] to database [%d%%]\r"%F,end='');G,H=TemplateOwner.objects.update_or_create(id=C[_C][_A],defaults=C[_D]);B+=1
+	print(f"Writing [Template Owner] to database {color_done}")
 def create_template(file_path):
 	A=[]
-	with open(os.path.join(file_path,'template.json'),'r')as C:A=json.load(C)
-	for B in A:D,E=Template.objects.update_or_create(id=B[_B][_A],defaults=B[_C])
-	print('Done Write data Template to database!')
+	with open(os.path.join(file_path,'template.json'),_B)as D:A=json.load(D)
+	B=1;E=len(A)
+	for C in A:F=B/E*100;print(f"Writing [Template] to database [%d%%]\r"%F,end='');G,H=Template.objects.update_or_create(id=C[_C][_A],defaults=C[_D]);B+=1
+	print(f"Writing [Template] to database {color_done}")
 def create_menu(file_path):
-	E='name';D='translation';C=[]
-	with open(os.path.join(file_path,'menu.json'),'r')as F:C=json.load(F)
+	F='name';E='translation';C=[]
+	with open(os.path.join(file_path,'menu.json'),_B)as G:C=json.load(G)
+	D=1;H=len(C)
 	for B in C:
-		A,I=Menu.objects.language(_A).update_or_create(id=B[_B][_A],defaults=B[_C]);A.menu_group.clear()
-		for G in B['m2m']['menu_group']:H=MenuGroup.objects.get(id=G);A.menu_group.add(H)
-		A.save();A.set_current_language('en');A.name=B[D][E]if B[D][E]else'';A.save()
-	print('Done Write data Menu to database!')
+		I=D/H*100;print(f"Writing [Menu] to database [%d%%]\r"%I,end='');A,L=Menu.objects.language(_A).update_or_create(id=B[_C][_A],defaults=B[_D]);A.menu_group.clear()
+		for J in B['m2m']['menu_group']:K=MenuGroup.objects.get(id=J);A.menu_group.add(K)
+		A.save();A.set_current_language('en');A.name=B[E][F]if B[E][F]else'';A.save();D+=1
+	print(f"Writing [Menu] to database {color_done}")
 def create_model_list(file_path):
 	A=[]
-	with open(os.path.join(file_path,'model_list.json'),'r')as C:A=json.load(C)
-	for B in A:D,E=ModelList.objects.update_or_create(id=B[_B][_A],defaults=B[_C])
-	print('Done Write data Model List to database!')
+	with open(os.path.join(file_path,'model_list.json'),_B)as D:A=json.load(D)
+	B=1;E=len(A)
+	for C in A:F=B/E*100;print(f"Writing [Model List] to database [%d%%]\r"%F,end='');G,H=ModelList.objects.update_or_create(id=C[_C][_A],defaults=C[_D]);B+=1
+	print(f"Writing [Model List] to database {color_done}")
+def create_menu_default(file_path):
+	A=[]
+	with open(os.path.join(file_path,'menu_default.json'),_B)as E:A=json.load(E)
+	C=1;F=len(A)
+	for B in A:G=C/F*100;print(f"Writing [Menu Default] to database [%d%%]\r"%G,end='');D,H=MenuDefault.objects.update_or_create(id=B[_C][_A],defaults=B[_D]);D.service_option=B['m2m']['service_option'];D.save();C+=1
+	print(f"Writing [Menu Default] to database {color_done}")
 def create_model_list_setting(file_path):
 	A=[]
-	with open(os.path.join(file_path,'model_list_setting.json'),'r')as C:A=json.load(C)
-	ModelListSetting.objects.all().delete()
-	for B in A:D,E=ModelListSetting.objects.update_or_create(id=B[_B][_A],defaults=B[_C])
-	print('Done Write data Model List Setting to database!')
+	with open(os.path.join(file_path,'model_list_setting.json'),_B)as D:A=json.load(D)
+	B=1;E=len(A);ModelListSetting.objects.all().delete()
+	for C in A:F=B/E*100;print(f"Writing [Model List Setting] to database [%d%%]\r"%F,end='');G,H=ModelListSetting.objects.update_or_create(id=C[_C][_A],defaults=C[_D]);B+=1
+	print(f"Writing [Model List Setting] to database {color_done}")
 def create_social_app(file_path):
 	A=[]
-	with open(os.path.join(file_path,'social_app.json'),'r')as C:A=json.load(C)
-	for B in A:D,E=SocialApp.objects.update_or_create(id=B[_B][_A],defaults=B[_C])
-	print('Done Write data Social App to database!')
-def create_core_data(apps,schema_monitor):A='db';create_template_owner(A);create_template(A);create_menu(A);create_model_list(A);create_model_list_setting(A);create_social_app(A);print('ALL Done...!')
-def update_core_data():A='db';create_template_owner(A);create_template(A);create_menu(A);create_model_list(A);create_model_list_setting(A);create_social_app(A);print('ALL Data Updated...!')
+	with open(os.path.join(file_path,'social_app.json'),_B)as D:A=json.load(D)
+	B=1;E=len(A)
+	for C in A:F=B/E*100;print(f"Writing [Social App] to database [%d%%]\r"%F,end='');G,H=SocialApp.objects.update_or_create(id=C[_C][_A],defaults=C[_D]);B+=1
+	print(f"Writing [Social App] to database {color_done}")
+def create_core_data(apps,schema_monitor):A='db';create_template_owner(A);create_template(A);create_menu(A);create_model_list(A);create_menu_default(A);create_model_list_setting(A);create_social_app(A);print('ALL Done...!')
+def update_core_data():A='db';create_template_owner(A);create_template(A);create_menu(A);create_model_list(A);create_menu_default(A);create_model_list_setting(A);create_social_app(A);print('ALL Data Updated...!')
--- engine/core/management/commands/createcoredata.py
+++ engine/core/management/commands/createcoredata.py
@@ -1,14 +1,17 @@
-_E=None
+_H='model_list_id'
+_G='service_option'
+_F=None
+_E='w'
 _D='defaults'
 _C='condition'
 _B='name'
 _A='id'
 import json,os
 from allauth.socialaccount.models import SocialApp
+from core.models import MenuDefault,ModelList,ModelListSetting,OptServiceType,Template,TemplateOwner
 from django.core.management.base import BaseCommand
 from django.db.models import OuterRef,Subquery
 from menu.models import Menu,MenuTranslation
-from core.models import ModelList,ModelListSetting,Template,TemplateOwner
 class Command(BaseCommand):
 	help='Create or Update File from database to file (This file will use in migrate process, read how to do that?)';file_path='db'
 	def info(A,message):A.stdout.write(message)
@@ -17,39 +20,49 @@ class Command(BaseCommand):
 		A=[];D=TemplateOwner.objects.all()
 		for C in D:E={_A:C.id};F={_B:C.name};A.append({_C:E,_D:F})
 		if A:
-			with open(os.path.join(B.file_path,'template_owner.json'),'w')as G:G.write(json.dumps(A))
+			with open(os.path.join(B.file_path,'template_owner.json'),_E)as G:G.write(json.dumps(A))
 		B.info('Done Write [template_owner.json], Total files write: {}'.format(len(A)))
 	def create_template(C):
 		B=[];D=Template.objects.all()
-		for A in D:E={_A:A.id};F={_B:A.name,'rel_path':A.rel_path,'is_frontend':A.is_frontend,'template_owner_id':A.template_owner.id if A.template_owner else _E,'service_option':A.service_option};B.append({_C:E,_D:F})
+		for A in D:E={_A:A.id};F={_B:A.name,'rel_path':A.rel_path,'is_frontend':A.is_frontend,'template_owner_id':A.template_owner.id if A.template_owner else _F,_G:A.service_option};B.append({_C:E,_D:F})
 		if B:
-			with open(os.path.join(C.file_path,'template.json'),'w')as G:G.write(json.dumps(B))
+			with open(os.path.join(C.file_path,'template.json'),_E)as G:G.write(json.dumps(B))
 		C.info('Done Write [template.json], Total files write: {}'.format(len(B)))
 	def create_menu(C):
 		B=[];E=Subquery(MenuTranslation.objects.filter(master_id=OuterRef(_A),language_code='en').values(_B));F=Menu.objects.language(_A).filter(kind=2).annotate(name_en=E)
 		for A in F:
-			G={_A:A.id};H={_B:A.name,'parent_id':A.parent.id if A.parent else _E,'link':A.link,'order_menu':A.order_menu,'icon':A.icon,'kind':A.kind,'is_visibled':A.is_visibled,'is_external':A.is_external,'is_new':A.is_new,'exclude_menu':A.exclude_menu};I={_B:A.name_en};D=[]
+			G={_A:A.id};H={_B:A.name,'parent_id':A.parent.id if A.parent else _F,'link':A.link,'order_menu':A.order_menu,'icon':A.icon,'kind':A.kind,'is_visibled':A.is_visibled,'is_external':A.is_external,'is_new':A.is_new,'exclude_menu':A.exclude_menu};I={_B:A.name_en};D=[]
 			for J in A.menu_group.all():D.append(J.id)
 			K={'menu_group':D};B.append({_C:G,_D:H,'translation':I,'m2m':K})
 		if B:
-			with open(os.path.join(C.file_path,'menu.json'),'w')as L:L.write(json.dumps(B))
+			with open(os.path.join(C.file_path,'menu.json'),_E)as L:L.write(json.dumps(B))
 		C.info('Done Write [menu.json], Total files write: {}'.format(len(B)))
 	def create_model_list(C):
 		B=[];D=ModelList.objects.all()
-		for A in D:E={_A:A.id};F={_B:A.name,'description':A.description,'menu_id':A.menu.id if A.menu else _E,'status':A.status};B.append({_C:E,_D:F})
+		for A in D:E={_A:A.id};F={_B:A.name,'description':A.description,'menu_id':A.menu.id if A.menu else _F,'status':A.status};B.append({_C:E,_D:F})
 		if B:
-			with open(os.path.join(C.file_path,'model_list.json'),'w')as G:G.write(json.dumps(B))
+			with open(os.path.join(C.file_path,'model_list.json'),_E)as G:G.write(json.dumps(B))
 		C.info('Done Write [model_list.json], Total files write: {}'.format(len(B)))
+	def convert_to_list(C,data):
+		A=[]
+		for B in data:A.append(int(B))
+		return A
+	def create_menu_default(B):
+		A=[];D=MenuDefault.objects.all()
+		for C in D:E={_G:B.convert_to_list(C.service_option)};F={_A:C.id};G={_H:C.model_list.id};A.append({_C:F,_D:G,'m2m':E})
+		if A:
+			with open(os.path.join(B.file_path,'menu_default.json'),_E)as H:H.write(json.dumps(A))
+		B.info('Done Write [menu_default.json], Total files write: {}'.format(len(A)))
 	def create_model_list_setting(C):
 		B=[];D=ModelListSetting.objects.all()
-		for A in D:E={_A:A.id};F={'model_list_id':A.model_list.id if A.model_list else _E,'template_id':A.template.id if A.template else _E,'image_width':A.image_width,'image_height':A.image_height};B.append({_C:E,_D:F})
+		for A in D:E={_A:A.id};F={_H:A.model_list.id if A.model_list else _F,'template_id':A.template.id if A.template else _F,'image_width':A.image_width,'image_height':A.image_height};B.append({_C:E,_D:F})
 		if B:
-			with open(os.path.join(C.file_path,'model_list_setting.json'),'w')as G:G.write(json.dumps(B))
+			with open(os.path.join(C.file_path,'model_list_setting.json'),_E)as G:G.write(json.dumps(B))
 		C.info('Done Write [model_list_setting.json], Total files write: {}'.format(len(B)))
 	def create_social_app(C):
 		B=[];D=SocialApp.objects.all()
 		for A in D:E={_A:A.id};F={'provider':A.provider,_B:A.name,'client_id':A.client_id,'secret':A.secret,'key':A.key};B.append({_C:E,_D:F})
 		if B:
-			with open(os.path.join(C.file_path,'social_app.json'),'w')as G:G.write(json.dumps(B))
+			with open(os.path.join(C.file_path,'social_app.json'),_E)as G:G.write(json.dumps(B))
 		C.info('Done Write [social_app.json], Total files write: {}'.format(len(B)))
-	def handle(A,*B,**C):A.create_template_owner();A.create_template();A.create_menu();A.create_model_list();A.create_model_list_setting();A.create_social_app();A.info('All Done ...')
+	def handle(A,*B,**C):A.create_template_owner();A.create_template();A.create_menu();A.create_model_list();A.create_menu_default();A.create_model_list_setting();A.create_social_app();A.info('All Done ...')
