8ef5d8a80990ef3e61c9a66c42604334b890deec
ione03 <suratiwan03@gmail.com>
2026-05-19 16:29:51 +0800

    fix footer and contact

--- engine/frontend/templatetags/custom_filters.py
+++ engine/frontend/templatetags/custom_filters.py
@@ -7,8 +7,8 @@ def phone_number(number):
 	B='-';A=number
 	if A:
 		G=['62','60','65','63','66','84']
-		if A[:2]in G:C=A[:2];D=A[2:4];E=A[4:7];F=A[7:10];H=A[10:];return'('+C+')'+' '+D+B+E+B+F+B+H
-		elif A:C=A[:3];D=A[3:6];E=A[6:9];F=A[9:];return'('+C+')'+' '+D+B+E+B+F
+		if A[:2]in G:C=A[:2];D=A[2:4];E=A[4:7];F=A[7:10];H=A[10:];return'(+'+C+')'+' '+D+B+E+B+F+B+H
+		elif A:C=A[:3];D=A[3:6];E=A[6:9];F=A[9:];return'(+'+C+')'+' '+D+B+E+B+F
 	return''
 @register.filter
 def replace_with(string,find_replace=',|_'):A,B=find_replace.split('|');return string.replace(A,B)
